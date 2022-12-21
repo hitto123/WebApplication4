e4b4c169b5bc5b57fb34c4523066f285adf0d2c5
hitto <hitto0316@gmail.com>
2022-12-21 10:21:30 +0800

    加入專案檔案。

---
+++ WebApplication4/Controllers/RegistersController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models.EFModels;
+
+namespace WebApplication4.Controllers
+{
+    public class RegistersController : Controller
+    {
+        private Model1 db = new Model1();
+
+        // GET: Registers
+        public ActionResult Index()
+        {
+            return View(db.Registers.ToList());
+        }
+
+        // GET: Registers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Register register = db.Registers.Find(id);
+            if (register == null)
+            {
+                return HttpNotFound();
+            }
+            return View(register);
+        }
+
+        // GET: Registers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Registers/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Name,Email")] Register register)
+        {
+            //驗證Email
+            var dataIndb = db.Registers.FirstOrDefault(x => x.Email == register.Email);
+            if (dataIndb != null) 
+            {                           //欄位//  //錯誤訊息
+                ModelState.AddModelError("Email", "此信箱已經註冊過");
+            }
+            //由程式直接加入時間
+            register.CreateTime = DateTime.Now;
+            
+            if (ModelState.IsValid)
+            {
+                db.Registers.Add(register);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(register);
+        }
+
+       
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication4/Models/EFModels/Register.cs
@@ -0,0 +1,21 @@
+namespace WebApplication4.Models.EFModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Register
+    {
+        public int id { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        public DateTime? CreateTime { get; set; }
+    }
+}
